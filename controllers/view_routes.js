ac1363920a868ada0c1b10b2e7da7487109197ae
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-08 22:26:29 -0400

    View routes like old work

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -19,9 +19,37 @@ router.get("/", async (req, res) => {
 
     posts = posts.map(t => t.get({ plain: true }));
 
-    res.render("index", {
+    res.render("home", {
         isHome: true,
         isLoggedIn: req.session.user_id,
         posts
     });
 });
+
+// Show Register Page
+router.get("/register", (req, res) => {
+    if (req.session.user_id) return res.redirect("/dashboard");
+
+    res.render("register", {
+        isRegister: true
+    });
+});
+
+// Show Dashboard Page
+router.get("/dashboard", isAuthenticated, async (req, res) => {
+
+    const user = await User.findByPk(req.session.user_id, {
+        include: Post
+    });
+
+    const posts = user.posts.map(t => t.get({ plain: true }));
+
+    // The user IS logged in
+    res.render("dashboard", {
+        isDashboard: true,
+        email: user.email,
+        posts
+    });
+});
+
+module.exports = router;
