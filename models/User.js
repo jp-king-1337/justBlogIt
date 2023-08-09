f26324b06aeeac4d77958810775432c3e970183b
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-08 22:07:11 -0400

    Built User model like in class - had to reference sequelize work

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require("sequelize");
+const { hash, compare } = require("bcrypt");
+const db = require("../db/connection");
+const Post = require("./Post");
+
+class User extends Model { }
+
+User.init({
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        validate: {
+            min: 6
+        }
+    }
+}, {
+    sequelize: db,
+    modelName: "user",
+    hooks: {
+        async beforeCreate(user) {
+            const hashPassword = await hash(user.password, 10);
+
+            user.password = hashPassword;
+        }
+    },
+});
+
+User.prototype.validatePass = async function (formPassword) {
+    const isValid = await compare(formPassword, this.password);
+
+    return isValid;
+}
+
+User.hasMany(Post);
+Post.belongsTo(User);
+
+module.exports = User;
