8e06e00b9747d31d65b9a691440ac19138294499
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-08-08 22:09:30 -0400

    Post is easy by comparison. Set min to 10 because I don't want any short posts, but don't want to go as high as 30 or anything. I guess Twitter had max set to 140 to get their short tweets back in the day.

---
+++ models/Post.js
@@ -0,0 +1,19 @@
+const { Model, DataTypes } = require("sequelize");
+const db = require("../db/connection");
+
+class Post extends Model {}
+
+Post.init({
+    text: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            min: 10
+        }
+    }
+}, {
+    sequelize: db,
+    modelName: "post"
+});
+
+module.exports = Post;
